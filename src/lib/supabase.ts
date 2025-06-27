2c38aa3a994ff10e2850f0e2140041989f471a6c
TahlilMahfuz <tahlilkfaiyaz@gmail.com>
2025-06-28 00:15:26 +0600

    Changed

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,11 @@
 import { createClient } from "@supabase/supabase-js/dist/module"
 
 const supabaseUrl = 'https://lspvkltuwmollpswiuxj.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzcHZrbHR1d21vbGxwc3dpdXhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwNDA2MTYsImV4cCI6MjA2NjYxNjYxNn0.NMJLUDvBhiEogeVLcPj4DTZ-QHCW8LXDPwFeVjTmoiE'
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseKey = process.env.SUPABASE_KEY
+if (!supabaseKey) {
+  throw new Error('SUPABASE_KEY environment variable is not set');
+}
+// const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey) 
 
 export { supabase }
